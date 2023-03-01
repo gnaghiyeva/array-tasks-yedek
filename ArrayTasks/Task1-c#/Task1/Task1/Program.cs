b65099173a3a74f42bf0afb38e301104ee68b91a
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2023-03-02 00:30:21 +0400

    task with c#

---
+++ ArrayTasks/Task1-c#/Task1/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] array = { 1, 2, 3, 4, 5 };
+int cem = 0;
+for(int i=0; i<array.Length; i++)
+{
+    if (array[i] % 2 != 0)
+    {
+        cem += array[i];
+    }
+
+}   
+  Console.WriteLine("cem = "+ cem);
---
+++ ArrayTasks/Task2-c#/Task2-c#/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array = { 1, 2, 3, 4, 5 };
+int cem = 0;
+int edediOrta;
+
+for (int i = 0; i < array.Length; i++)
+{
+    cem += array[i];
+
+}
+
+edediOrta = cem / array.Length;
+
+Console.WriteLine("ededi orta = " + edediOrta);
+
